5a00f5416cd3f1ff2ad1a847c4f40b5619163287
n4to4 <n4to4k@gmail.com>
2022-06-09 18:19:27 +0900

    wip

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -8,16 +8,21 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
   return (
     <>
       <header>
-        <h1>Redwood Blog</h1>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
         <nav>
           <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
             <li>
               <Link to={routes.about()}>About</Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>{children} </main>
+      <main>{children}</main>
     </>
   )
 }
